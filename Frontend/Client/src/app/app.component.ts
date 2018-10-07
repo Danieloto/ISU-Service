1f938cff2ca81863856b782e79899279f191c421
Daniel Laracuenta <dalc@iastate.edu>
2018-10-07 01:43:36 -0500

    Updates

--- Frontend/Client/src/app/app.component.ts
+++ Frontend/Client/src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
 import { Card } from './models/card.model';
 import { OfferingService } from './offering.service';
-<<<<<<< HEAD
-import { UserService } from './user.service';
-=======
-
->>>>>>> 84d87d761a488429c9f339113f6146e5a6f036eb
 
 @Component({
   selector: 'app-root',
